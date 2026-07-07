cefb023a9520bf63556ee4d9c20545996d1ec20e
LCF104 <99516671+LCF104@users.noreply.github.com>
2026-07-07 15:14:43 +0800

    Add AKShare placeholder provider

---
+++ app/providers/akshare_provider.py
@@ -0,0 +1,8 @@
+class AKShareProvider:
+    """Optional helper provider placeholder.
+
+    AKShare is intentionally not used as the only primary source in v1.
+    Install and wire it here later when you want public-data enrichment.
+    """
+
+    enabled = False
